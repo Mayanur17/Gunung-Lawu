2bb5ba80fdd8fdb94bb816d6ddc23b4daacc7c45
Mayanur17 <mayanurrr77@gmail.com>
2025-07-23 19:21:06 +0700

    update

--- resources/views/pendaki/cemorosewu.blade.php
+++ resources/views/pendaki/cemorosewu.blade.php
@@ -95,4 +95,36 @@
         </div>
     </div>
 @endif
+{{-- Google Maps untuk masing-masing jalur --}}
+<div class="maps-section">
+    <h4>Lokasi Jalur di Google Maps</h4>
+    <div class="map-embed" style="width: 100%; height: 400px;">
+        @php
+            $jalurNama = strtolower($jalur->jalur_pendakian);
+        @endphp
+
+        @if($jalurNama === 'cemoro kandang')
+        <iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63272.044246203346!2d111.14243932586993!3d-7.628953230059511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e798c1ae031bb7f%3A0x913104fec197ca69!2sBase%20Camp%20AGL%20Cemoro%20Kandang%2C%20Gn.%20Lawu!5e0!3m2!1sid!2sid!4v1753271268430!5m2!1sid!2sid" 
+    width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" 
+    referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+
+        @elseif($jalurNama === 'cemoro sewu')
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63272.09573280656!2d111.14243909593917!3d-7.628605137029124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e798df1913548dd%3A0xe95d6dcbaf046c67!2sCemoro%20Sewu!5e0!3m2!1sid!2sid!!5m2!1sid!2sid"
+                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        @elseif($jalurNama === 'cetho')
+            <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.8124347841735!2d111.15374597319109!3d-7.595382525054982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e798b673d72a09d%3A0xae4de0b35464ae7!2sBasecamp%20Lawu%20via%20Ceto%20(Barokah)!5e0!3m2!1sid!2sid!4v1753273044059!5m2!1sid!2sid" 
+                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        @else
+            <p style="color:red;">Peta tidak tersedia untuk jalur ini.</p>
+        @endif
+    </div>
+</div>
 @endsection
